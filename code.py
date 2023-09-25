d6ad34e31478afa7f8c9ce3a1b4df517c5cd1155
User <user@hackerrank.workspace>
2023-09-25 17:50:45 +0000

    program to find fibonacci series

---
+++ code.py
@@ -0,0 +1,6 @@
+def fibonacci(n):
+    fib_series = [0, 1]
+    while len(fib_series) < n:
+        next_num = fib_series[-1] + fib_series[-2]
+        fib_series.append(next_num)
+    return fib_series
